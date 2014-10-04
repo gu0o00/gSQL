3b4391c6801f8d68d54bb64deb16d63147d0dd9f
g975291783 <g975291783@163.com>
2014-10-04 14:41:41 +0800

    第一次提交，定义了界面

---
+++ gSQL/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace gSQL
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.inputBox = new System.Windows.Forms.RichTextBox();
+			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.outputBox = new System.Windows.Forms.RichTextBox();
+			this.toolStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// inputBox
+			// 
+			this.inputBox.BackColor = System.Drawing.Color.Black;
+			this.inputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.inputBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+			this.inputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.inputBox.Location = new System.Drawing.Point(0, 0);
+			this.inputBox.Name = "inputBox";
+			this.inputBox.Size = new System.Drawing.Size(688, 142);
+			this.inputBox.TabIndex = 0;
+			this.inputBox.Text = "";
+			this.inputBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.inputBox_KeyUp);
+			// 
+			// toolStrip1
+			// 
+			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+			this.toolStrip1.Name = "toolStrip1";
+			this.toolStrip1.Size = new System.Drawing.Size(688, 25);
+			this.toolStrip1.TabIndex = 1;
+			this.toolStrip1.Text = "toolStrip1";
+			// 
+			// toolStripButton1
+			// 
+			this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+			this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+			this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton1.Name = "toolStripButton1";
+			this.toolStripButton1.Size = new System.Drawing.Size(120, 22);
+			this.toolStripButton1.Text = "以后在这里添加功能";
+			this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.outputBox);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.inputBox);
+			this.splitContainer1.Size = new System.Drawing.Size(688, 399);
+			this.splitContainer1.SplitterDistance = 253;
+			this.splitContainer1.TabIndex = 2;
+			// 
+			// outputBox
+			// 
+			this.outputBox.BackColor = System.Drawing.Color.Black;
+			this.outputBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.outputBox.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.outputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+			this.outputBox.Location = new System.Drawing.Point(0, 0);
+			this.outputBox.Name = "outputBox";
+			this.outputBox.ReadOnly = true;
+			this.outputBox.Size = new System.Drawing.Size(688, 253);
+			this.outputBox.TabIndex = 0;
+			this.outputBox.Text = "";
+			this.outputBox.WordWrap = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(688, 424);
+			this.Controls.Add(this.splitContainer1);
+			this.Controls.Add(this.toolStrip1);
+			this.Name = "Form1";
+			this.Text = "gSQL 1.0";
+			
+			this.toolStrip1.ResumeLayout(false);
+			this.toolStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox inputBox;
+		private System.Windows.Forms.ToolStrip toolStrip1;
+		private System.Windows.Forms.ToolStripButton toolStripButton1;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.RichTextBox outputBox;
+	}
+}
+
---
+++ gSQL/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gSQL
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void toolStripButton1_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("功能尚未添加");
+		}
+
+		private void inputBox_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (sender.ToString().Contains("RichTextBox") &&
+				e.KeyCode.ToString().Equals("Return") &&
+				this.inputBox.Text.Trim(new char[] { '\n' }).EndsWith(";"))
+			{
+				string[] sql = this.inputBox.Text.Split(';');
+				String asql = (sql[line++] + ";").Replace('\n', ' ').TrimStart();
+				outputBox.Text += asql+"\n";
+			}
+		}
+		static Int32 line = 0;
+	}
+}
